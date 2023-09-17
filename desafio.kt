86a595ccac173acb1b4123dbe33bff6b7b7552a2
estefaniapinheiro12 <120748637+estefaniapinheiro12@users.noreply.github.com>
2023-09-16 21:29:23 -0300

    Ajustes.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,50 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+data class Usuario(val nome: String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class ConteudoEducacional(val nome: String, val duracao: Int, val nivel: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+  // Aqui na função matricular, tem uma verificação se o usuário já possui uma matricula.
+    fun matricular(vararg usuarios: Usuario) {
+        for (usuario in usuarios) {
+            if (!inscritos.contains(usuario)) {
+                inscritos.add(usuario)
+                println("${usuario.nome} matriculado na formação $nome")
+            } else {
+                println("${usuario.nome} já está matriculado na formação $nome")
+            }
+        }
+    }
+// Aqui também é feita uma verificação se o usuário está matriculado.
+    fun cancelarMatricula(usuario: Usuario) {
+        if (inscritos.contains(usuario)) {
+            inscritos.remove(usuario)
+            println("${usuario.nome} desmatriculado da formação $nome")
+        } else {
+            println("${usuario.nome} não estava matriculado na formação $nome")
+        }
+    }
+//Aqui verifica se existe ou não uma matricula
+    fun verificarMatricula(usuario: Usuario): Boolean {
+        return inscritos.contains(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Miguel")
+    val usuario2 = Usuario("Ana")
+    val usuario3 = Usuario("Inácio")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 6, "Básico")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 8, "Intermediário")
+    val conteudo3 = ConteudoEducacional("Desenvolvimento Android", 12, "Avançado")
+
+    val formacao = Formacao("Formação Kotlin", listOf(conteudo1, conteudo2, conteudo3))
+
+    formacao.matricular(usuario1, usuario2, usuario3)
+    formacao.cancelarMatricula(usuario2)
+
+    println("Verificar matrícula de Ana: ${formacao.verificarMatricula(usuario2)}")
+    println("Verificar matrícula de Inácio: ${formacao.verificarMatricula(usuario3)}")
 }
